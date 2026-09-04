bf7d390c77c93a9e3ba8ad57c0d8236a03e255be
And Rus <raa@m4bank.com>
2026-09-04 15:57:57 +0300

    Add project files

---
+++ app/components/news/NewsList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const newsItems = ref([])
+</script>
+
+<template>
+  <div v-for="news in newsItems">
+    <NewsItem :news="news" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+import Aura from '@primeuix/themes/aura'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@primevue/nuxt-module', '@pinia/nuxt'],
+  primevue: {
+    components: {
+      prefix: 'Prime'
+    },
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
